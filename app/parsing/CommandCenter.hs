00ff036f053303fd0fcf44bbcd3a8de3f6e99986
contraposition <tumblrpod1@gmail.com>
2024-02-29 10:16:08 +0100

    init: hub for recieving commands from user

---
+++ app/parsing/CommandCenter.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE OverloadedStrings #-}  -- allows "string literals" to be Text
+
+module Parsing.CommandCenter where
+
+import           Data.Text (Text)
+
+
+--recieve :: Message -> Text
+--recieve = ""
