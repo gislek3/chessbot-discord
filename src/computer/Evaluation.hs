c6fa2c6a8071ddec6b66a137c117c200412d1147
contraposition <tumblrpod1@gmail.com>
2024-04-05 03:21:03 +0200

    Fixed module name in Evaluation

--- src/computer/Evaluation.hs
+++ src/computer/Evaluation.hs
@@ -1,5 +1,12 @@
-module Computer.Evaluation where
+module Computer.Evaluation (evaluate) where
+
+import Chess.Board
+import Chess.Board
+import Chess.Board (Board)
 
 --Hello world!
 --Should take in a board and score it using helpers, I would prefer that this is ALL that this
 --module does, and I would like for it to only export a single function! that would be neat
+
+evaluate :: Board -> Int
+evaluate b = 0
