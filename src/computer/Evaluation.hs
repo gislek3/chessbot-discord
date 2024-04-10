ac2205c1af18efc63c8317be938ddd3be8964dff
contraposition <tumblrpod1@gmail.com>
2024-04-10 16:57:17 +0200

    Slight improvements to evaluations, included debug version of evaluae function

--- src/computer/Evaluation.hs
+++ src/computer/Evaluation.hs
@@ -1,4 +1,4 @@
-module Computer.Evaluation (evaluate) where
+module Computer.Evaluation (evaluate, debugEvaluation) where
 
 import Chess.Board
 import Chess.Piece
@@ -11,13 +11,14 @@ import Chess.Board (getAllColorMoves)
 type BoardValue = Int
 type ValueMap = M.Map Square BoardValue
 
---TODO:  am not consdiering the AMOUNT of squares, total piece activity
+
+--TODO:  am not consdiering the AMOUNT of squares, total piece activity... or am I?????
 squareValue :: Square -> Int
 squareValue s = fromMaybe 0 (M.lookup s squareValueMap)
     where
         squareValueMap = M.fromList [((x, y), calculateValue x y) | x <- [0..7], y <- [0..7]]
         centers = [(3, 3), (3, 4), (4, 3), (4, 4)]  -- The central four squares: d4, e4, d5, e5
-        baseValue = 10   -- Base value for centrality
+        baseValue = 8   -- Base value for centrality
         calculateValue x y = max 0 $ baseValue - minimum (map (distanceFromCenter x y) centers)
         distanceFromCenter x0 y0 center = abs (x0 - fst center) + abs (y0 - snd center)
 
@@ -31,19 +32,20 @@ evaluateMaterial :: Board -> Int
 evaluateMaterial b = sum $ map pieceValue pieces where
     pieces = getAllPieces b
     pieceValue (Piece pieceType c _) = let m = if c==White then 1 else (-1) in case pieceType of
-        Pawn   -> 1*m
-        Knight -> 3*m
-        Bishop -> 3*m
-        Rook   -> 5*m
-        Queen  -> 9*m
+        Pawn   -> 35*m
+        Knight -> 100*m
+        Bishop -> 130*m
+        Rook   -> 225*m
+        Queen  -> 450*m
         King   -> 0*m
 
+
 evaluateExposure :: Square -> Board -> Int
-evaluateExposure s b = if isEmpty (lookupB s b) then 1 else 0
+evaluateExposure s b = if isEmpty (lookupB s b) then 3 else 0
 
 
 isThreatTo :: Move -> Square -> Int
-isThreatTo m s = if new_square m==s then 1 else 0
+isThreatTo m s = if new_square m==s then 6 else 0
 
 calculateTotalThreats :: Board -> Int
 calculateTotalThreats b = do
@@ -53,7 +55,7 @@ calculateTotalThreats b = do
     let blackMoves = S.toList $ getAllColorMoves Black b
     let whiteThreats = [isThreatTo move square | move <- blackMoves, square <- whiteKingSqs]
     let blackThreats = [isThreatTo move square | move <- whiteMoves, square <- blackKingSqs]
-    length whiteThreats + (-1) * length blackThreats
+    sum whiteThreats + (-1)*sum blackThreats
 
 
 
@@ -64,9 +66,14 @@ evaluateKingSafety b = do
     calculateTotalThreats b + blackExposed + whiteExposed
 
 
---Hello world!
---Should take in a board and score it using helpers, I would prefer that this is ALL that this
---module does, and I would like for it to only export a single function! that would be neat
+debugEvaluation :: Board -> M.Map String Int
+debugEvaluation b = M.fromList [
+        ("Activity score", evaluateSquares b),
+        ("Material score", evaluateMaterial b),
+        ("King threat score", calculateTotalThreats b),
+        ("King exposure score", evaluateKingSafety b-(calculateTotalThreats b)),
+        ("Total score", evaluate b)
+    ]
 
 --TODO: replace with Reader-monad to save less parameters bro lets gooooo!!!!
 evaluate :: Board -> Int
--- test/EvaluationTest.hs
+++ test/EvaluationTest.hs
@@ -6,6 +6,7 @@ import Chess.Board
 import Chess.Piece
 import Computer.Evaluation
 import Data.Maybe (isJust, fromJust, isNothing)
+import qualified Data.Map as M
 
 
 b :: Board
@@ -16,11 +17,13 @@ testBasicEvaluation = TestCase $ do
   assertBool "Starting position is evaluated to be equal" (evaluate b==0)
   let e2_e4 = makeMove (Move (Piece Pawn White False) (4,1) (4,3)) b
   assertBool "e4 is a valid move" (isJust e2_e4)
-  assertEqual "White has a slight advantage after opening move" (evaluate $ fromJust e2_e4) (0)
+  assertNotEqual "White has a slight advantage after opening move" (evaluate $ fromJust e2_e4) (0)
+  let removed_knight = clear (1,0) $ fromJust e2_e4
+  assertEqual "debug" M.empty (debugEvaluation removed_knight)
 
   
 
 tests :: Test
 tests = TestList [
-
+    TestLabel "testBasicEvaluation" testBasicEvaluation
     ]
